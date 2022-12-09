f5c07c038d87624d8cfcdf1ab75dff92220e2ca4
CeSiumUA <fedir.katushonok@gmail.com>
2022-12-09 22:57:36 +0200

    Added Bluetooth control

--- src/main.c
+++ src/main.c
@@ -3,10 +3,16 @@
 #include "servo.h"
 #include "usart.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define COMMAND_BUFFER_LENGTH 4
 
+#define USE_BLUETOOTH
+
 char command_buffer[COMMAND_BUFFER_LENGTH];
+char last_receive_command[COMMAND_BUFFER_LENGTH];
 
 void process_control_buttons(void);
 
@@ -21,11 +27,22 @@ int main(void){
 
     init_irqs();
 
-    initialize_rotating_servo();
+    #ifdef USE_BUTTONS
+    initialize_control_buttons();
+    #endif
 
+    #ifdef USE_SERVO
+    initialize_rotating_servo();
+    #endif
+
+    #ifdef USE_BLUETOOTH
     initialize_usart_bt((uint32_t)command_buffer, COMMAND_BUFFER_LENGTH);
+    #endif
 
     while(1){
+        #ifdef USE_BUTTONS
+        process_control_buttons();
+        #endif
     }
 }
 
@@ -49,26 +66,37 @@ void TIM2_IRQHandler(void){
     TIM2 -> SR &=~TIM_SR_UIF;
 }
 
-void DMA1_Stream5_IRQHandler(void){
-    DMA1 -> HIFCR |= (DMA_HIFCR_CFEIF5 | DMA_HIFCR_CDMEIF5 | DMA_HIFCR_CTEIF5 | DMA_HIFCR_CHTIF5 | DMA_HIFCR_CTCIF5);
+void DMA2_Stream2_IRQHandler(void){
+    DMA2 -> HIFCR |= (DMA_LIFCR_CFEIF2 | DMA_LIFCR_CDMEIF2 | DMA_LIFCR_CTEIF2 | DMA_LIFCR_CHTIF2 | DMA_LIFCR_CTCIF2);
 
-    int mode = command_buffer[0] - '0';
-    float angle = atoff(command_buffer + 1);
+    bool is_command_new = memcmp(command_buffer, last_receive_command, COMMAND_BUFFER_LENGTH);
+    if(is_command_new){
+        usart_bt_write((uint32_t)command_buffer, COMMAND_BUFFER_LENGTH);
+        memmove(last_receive_command, command_buffer, COMMAND_BUFFER_LENGTH);
+    }
+    //int mode = command_buffer[0] - '0';
+    //float angle = atoff(command_buffer + 1);
+    #ifdef USE_SERVO
     if(mode){
         set_horizontal_rotation_angle(angle);
     }
     else{
         set_vertical_rotation_angle(angle);
     }
+    #endif
 }
 
-void DMA1_Stream6_IRQHandler(void){
-    DMA1 -> HIFCR |= (DMA_HIFCR_CFEIF6 | DMA_HIFCR_CDMEIF6 | DMA_HIFCR_CTEIF6 | DMA_HIFCR_CHTIF6 | DMA_HIFCR_CTCIF6);
+void DMA2_Stream6_IRQHandler(void){
+    DMA2 -> HIFCR |= (DMA_HIFCR_CFEIF6 | DMA_HIFCR_CDMEIF6 | DMA_HIFCR_CTEIF6 | DMA_HIFCR_CHTIF6 | DMA_HIFCR_CTCIF6);
+
+    DMA2_Stream6 -> CR &=~DMA_SxCR_EN;
 }
 
 void EXTI9_5_IRQHandler(void){
+    #ifdef USE_SERVO
     if(EXTI -> PR & EXTI_PR_PR6){
         EXTI -> PR |= EXTI_PR_PR6;
         process_mode_selection();
     }
+    #endif
 }
--- src/usart.c
+++ src/usart.c
@@ -1,6 +1,6 @@
 #include "usart.h"
 
-#define USART_BLUETOOTH_SPEED 115200
+#define USART_BLUETOOTH_SPEED 9600
 
 static uint16_t compute_bd(uint32_t periph_clk, uint32_t baud_rate){
     return ((periph_clk + (baud_rate/2U))/baud_rate);
@@ -8,73 +8,71 @@ static uint16_t compute_bd(uint32_t periph_clk, uint32_t baud_rate){
 
 void initialize_usart(void){
 
-    USART_DeInit(USART2);
+    USART_DeInit(USART6);
 
     RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    RCC -> APB1ENR |= RCC_APB1ENR_USART2EN;
+    RCC -> APB2ENR |= RCC_APB2ENR_USART6EN;
 
-    GPIOA -> MODER &=~GPIO_MODER_MODER2_0;
-    GPIOA -> MODER |= GPIO_MODER_MODER2_1;
+    GPIOA -> MODER &=~GPIO_MODER_MODER11_0;
+    GPIOA -> MODER |= GPIO_MODER_MODER11_1;
 
-    GPIOA -> MODER &=~GPIO_MODER_MODER3_0;
-    GPIOA -> MODER |= GPIO_MODER_MODER3_1;
+    GPIOA -> MODER &=~GPIO_MODER_MODER12_0;
+    GPIOA -> MODER |= GPIO_MODER_MODER12_1;
 
-    GPIOA -> AFR[0] |= (GPIO_AF_USART2 << 8);
-    GPIOA -> AFR[0] |= (GPIO_AF_USART2 << 12);
-
-    RCC -> APB1ENR |= RCC_APB1ENR_USART2EN;
+    GPIOA -> AFR[1] |= (GPIO_AF_USART6 << 12);
+    GPIOA -> AFR[1] |= (GPIO_AF_USART6 << 16);
 
     uint16_t baud_rate = compute_bd(HSI_VALUE, USART_BLUETOOTH_SPEED);
 
-    USART2 -> BRR = baud_rate;
+    USART6 -> BRR = baud_rate;
 
-    USART2 -> CR3 |= USART_CR3_DMAT;
-    USART2 -> CR3 |= USART_CR3_DMAR;
+    USART6 -> CR3 |= USART_CR3_DMAT;
+    USART6 -> CR3 |= USART_CR3_DMAR;
 
-    USART2 -> CR1 |= (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);
+    USART6 -> CR1 |= (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);
 }
 
 void initialize_dma(uint32_t dst, uint32_t len){
-    DMA_DeInit(DMA1_Stream5);
-    DMA_DeInit(DMA1_Stream6);
+    DMA_DeInit(DMA2_Stream2);
+    DMA_DeInit(DMA2_Stream6);
 
-    RCC -> AHB1ENR |= RCC_AHB1ENR_DMA1EN;
+    RCC -> AHB1ENR |= RCC_AHB1ENR_DMA2EN;
 
-    DMA1_Stream5 -> CR &=~ DMA_SxCR_EN;
-    while(DMA1_Stream5 -> CR & DMA_SxCR_EN){}
+    DMA2_Stream2 -> CR &=~ DMA_SxCR_EN;
+    while(DMA2_Stream2 -> CR & DMA_SxCR_EN){}
 
-    DMA1_Stream6 -> CR &=~ DMA_SxCR_EN;
-    while(DMA1_Stream6 -> CR & DMA_SxCR_EN){}
+    DMA2_Stream6 -> CR &=~ DMA_SxCR_EN;
+    while(DMA2_Stream6 -> CR & DMA_SxCR_EN){}
 
-    DMA1 -> HIFCR |= (DMA_HIFCR_CFEIF6 | DMA_HIFCR_CDMEIF6 | DMA_HIFCR_CTEIF6 | DMA_HIFCR_CHTIF6 | DMA_HIFCR_CTCIF6);
-    DMA1 -> HIFCR |= (DMA_HIFCR_CFEIF5 | DMA_HIFCR_CDMEIF5 | DMA_HIFCR_CTEIF5 | DMA_HIFCR_CHTIF5 | DMA_HIFCR_CTCIF5);
+    DMA2 -> HIFCR |= (DMA_HIFCR_CFEIF6 | DMA_HIFCR_CDMEIF6 | DMA_HIFCR_CTEIF6 | DMA_HIFCR_CHTIF6 | DMA_HIFCR_CTCIF6);
+    DMA2 -> HIFCR |= (DMA_LIFCR_CFEIF2 | DMA_LIFCR_CDMEIF2 | DMA_LIFCR_CTEIF2 | DMA_LIFCR_CHTIF2 | DMA_LIFCR_CTCIF2);
 
-    DMA1_Stream5 -> PAR |= (uint32_t)&USART2 -> DR;
-    DMA1_Stream6 -> PAR |= (uint32_t)&USART2 -> DR;
+    DMA2_Stream2 -> PAR |= (uint32_t)&USART6 -> DR;
+    DMA2_Stream6 -> PAR |= (uint32_t)&USART6 -> DR;
 
-    DMA1_Stream5 -> M0AR = dst;
-    DMA1_Stream5 -> NDTR = len;
+    DMA2_Stream2 -> M0AR = dst;
+    DMA2_Stream2 -> NDTR = len;
 
-    DMA1_Stream5 -> CR |= DMA_SxCR_CHSEL_2;
-    DMA1_Stream6 -> CR |= DMA_SxCR_CHSEL_2;
+    DMA2_Stream2 -> CR |= (DMA_SxCR_CHSEL_2 | DMA_SxCR_CHSEL_0);
+    DMA2_Stream6 -> CR |= (DMA_SxCR_CHSEL_2 | DMA_SxCR_CHSEL_0);
 
-    DMA1_Stream5 -> CR |= DMA_SxCR_CIRC;
-    DMA1_Stream6 -> CR |= DMA_SxCR_DIR_0;
+    DMA2_Stream2 -> CR |= DMA_SxCR_CIRC;
+    DMA2_Stream6 -> CR |= DMA_SxCR_DIR_0;
 
-    DMA1_Stream5 -> CR |= DMA_SxCR_MINC;
-    DMA1_Stream6 -> CR |= DMA_SxCR_MINC;
+    DMA2_Stream2 -> CR |= DMA_SxCR_MINC;
+    DMA2_Stream6 -> CR |= DMA_SxCR_MINC;
 
-    DMA1_Stream5 -> CR |= DMA_SxCR_TCIE;
-    DMA1_Stream6 -> CR |= DMA_SxCR_TCIE;
+    DMA2_Stream2 -> CR |= DMA_SxCR_TCIE;
+    DMA2_Stream6 -> CR |= DMA_SxCR_TCIE;
 
-    DMA1_Stream5 -> FCR = 0;
-    DMA1_Stream6 -> FCR = 0;
+    DMA2_Stream2 -> FCR = 0;
+    DMA2_Stream6 -> FCR = 0;
 
-    DMA1_Stream5 -> CR |= DMA_SxCR_EN;
+    DMA2_Stream2 -> CR |= DMA_SxCR_EN;
 
-    NVIC_EnableIRQ(DMA1_Stream5_IRQn);
-    NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+    NVIC_EnableIRQ(DMA2_Stream2_IRQn);
+    NVIC_EnableIRQ(DMA2_Stream6_IRQn);
 }
 
 void initialize_usart_bt(uint32_t dst, uint32_t len){
@@ -86,9 +84,9 @@ void initialize_usart_bt(uint32_t dst, uint32_t len){
 
 void usart_bt_write(uint32_t src, uint32_t dtlen){
     
-    DMA1_Stream6 -> M0AR = src;
+    DMA2_Stream6 -> M0AR = src;
 
-    DMA1_Stream6 -> NDTR = dtlen;
+    DMA2_Stream6 -> NDTR = dtlen;
 
-    DMA1_Stream6 -> CR |= DMA_SxCR_EN;
+    DMA2_Stream6 -> CR |= DMA_SxCR_EN;
 }
